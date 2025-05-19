fe3614e588baddf0514a8efda3babf00d1eab52b
kIeRa5578NM <spartan.ten_0e@icloud.com>
2025-05-19 08:38:00 +0100

    Add tests to ensure the file's security compliance

--- 76Ij/seg/MO357R/academic-advancement_FruKMk_1744348270.txt.php
+++ 76Ij/seg/MO357R/academic-advancement_FruKMk_1744348270.txt.php
@@ -202,7 +202,6 @@
 		</footer>
 		<!-- jQuery -->
 		<script src="js/jquery/jquery-2.2.4.min.js"></script>
-		<!-- Bootstrap -->
 		<script src="js/bootstrap/popper.min.js"></script>
 		<script src="js/bootstrap/bootstrap.min.js"></script>
 		<!-- Owl-coursel -->
