f20145688cc1f59e43b8b8308dede607ab7986f3
kIeRa5578NM <spartan.ten_0e@icloud.com>
2025-05-21 10:01:18 +0100

    Add comments to explain complex logic in the file

--- F4_uy/T1ml/WBd8jn/Yuq/google-maps_DkohB_1744304822.data.js
+++ F4_uy/T1ml/WBd8jn/Yuq/google-maps_DkohB_1744304822.data.js
@@ -329,7 +329,6 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
     var container = config.container;
     var shadowCanvas = this.shadowCanvas = document.createElement('canvas');
     var canvas = this.canvas = config.canvas || document.createElement('canvas');
-    var renderBoundaries = this._renderBoundaries = [10000, 10000, 0, 0];
 
     var computed = getComputedStyle(config.container) || {};
 
